becc631d22356cd5f3d13cf08ff8a1b0c56dc9f7
Panakotta00 <panakotta00@gmail.com>
2020-03-16 22:52:36 +0100

    Porting Fueled Lamps to C++ & fix some stuff

---
+++ Source/LightItUp/FueledLampBase.cpp
@@ -0,0 +1,37 @@
+#include "FueledLampBase.h"
+#include "FGActorRepresentationManager.h"
+#include "Components/SpotLightComponent.h"
+#include "UnrealNetwork.h"
+
+AFueledLampBase::AFueledLampBase() {
+	PowerConnectionProduction = CreateDefaultSubobject<UFGPowerConnectionComponent>("PowerConnectionProduction");
+	PowerConnectionProduction->SetIsHidden(true);
+
+	PowerConnectionConsumption = CreateDefaultSubobject<UFGPowerConnectionComponent>("PowerConnectionConsumption");
+	PowerConnectionConsumption->SetIsHidden(true);
+}
+
+void AFueledLampBase::BeginPlay() {
+	Super::BeginPlay();
+
+	if (HasAuthority()) {
+		PowerConnectionProduction->SetPowerInfo(GetPowerInfo());
+		PowerConnectionConsumption->SetPowerInfo(GetPowerInfo());
+		PowerConnectionConsumption->AddHiddenConnection(PowerConnectionProduction);
+	}
+}
+
+void AFueledLampBase::TickActor(float DeltaTime, ELevelTick TickType, FActorTickFunction& ThisTickFunction) {
+	Super::TickActor(DeltaTime, TickType, ThisTickFunction);
+
+	TArray<UActorComponent*> lights = GetComponentsByClass(ULightComponent::StaticClass());
+	bool bLampOn = HasPower() && !IsProductionPaused();
+	for (auto& light : lights) {
+		Cast<ULightComponent>(light)->SetVisibilitySML(bLampOn, true);
+	}
+}
+
+void AFueledLampBase::Factory_Tick(float dt) {
+	if (HasAuthority())	GetPowerInfo()->SetTargetConsumption(GetPendingPotential() * mPowerProduction / 4.1);
+	Super::Factory_Tick(dt);
+}
